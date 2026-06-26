f6d3df6435f3bcd7207290fee0c65accac0acc18
Nir Oz <47395913+Nir-0z@users.noreply.github.com>
2026-06-26 16:29:20 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="מערכת הדשבורדים - i24NEWS")
+st.markdown("<h1 style='text-align: right;'>📺 פורטל הנתונים והמחקר - i24NEWS</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: right;'>ברוכים הבאים. בחרו את הדשבורד המבוקש מתפריט הצד.</p>", unsafe_allow_html=True)
